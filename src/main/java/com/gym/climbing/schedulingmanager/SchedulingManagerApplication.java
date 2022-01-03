71e5b1f036cd450194f72c1f8a03bb398593cce8
Diogo Simão <diogo.simao@vodafone.com>
2022-01-03 00:40:11 +0000

    First commit with Spring Boot initializer

---
+++ src/main/java/com/gym/climbing/schedulingmanager/SchedulingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gym.climbing.schedulingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/climbing/schedulingmanager/SchedulingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.climbing.schedulingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
